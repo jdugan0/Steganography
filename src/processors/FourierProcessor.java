28f248ec0bb657699d97449db2e521b0a031668a
jdugan0 <justin.d.dugan@gmail.com>
2025-02-21 14:28:52 -0500

    return decode

--- src/processors/FourierProcessor.java
+++ src/processors/FourierProcessor.java
@@ -3,7 +3,11 @@ package processors;
 import filereader.Image;
 
 public class FourierProcessor {
-    public static Image encoded(Image image) {
-        return image;
+    public static Image encode(Image source, Image encode) {
+        return source;
+    }
+
+    public static Image decode(Image decode) {
+        return decode;
     }
 }
