c755c83f9f949582edc30f2953dbdd4b59c08d38
jdugan0 <justin.d.dugan@gmail.com>
2025-02-21 14:00:56 -0500

    added image type

--- src/filereader/FileReader.java
+++ src/filereader/FileReader.java
@@ -5,10 +5,16 @@ import java.io.File;
 import javax.imageio.ImageIO;
 
 public class FileReader {
-    public static Image readImage(String relativeFilePath) {
+    public enum ImageType {
+        Encode,
+        Decode,
+        Storage
+    }
+
+    public static Image readImage(ImageType type, String relativeFilePath) {
         try {
 
-            String basePath = new File("").getAbsolutePath() + File.separator + "Images";
+            String basePath = new File("").getAbsolutePath() + File.separator + "Images" + File.separator + type;
             String fullPath = basePath + File.separator + relativeFilePath;
             System.out.println("Reading image from: " + fullPath);
             BufferedImage img = ImageIO.read(new File(fullPath));
