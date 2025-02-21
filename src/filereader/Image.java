000c03963ecc5a6326c8b1f375a3c63eeb559d78
jdugan0 <justin.d.dugan@gmail.com>
2025-02-21 15:45:31 -0500

    working DFT!!!

--- src/filereader/Image.java
+++ src/filereader/Image.java
@@ -2,6 +2,8 @@ package filereader;
 
 import java.awt.image.BufferedImage;
 
+import processors.Complex;
+
 public class Image {
     public final int[][] r;
     public final int[][] g;
@@ -9,15 +11,15 @@ public class Image {
     public final int width;
     public final int height;
 
-    public Image(BufferedImage image){
+    public Image(BufferedImage image) {
         width = image.getWidth();
         height = image.getHeight();
         r = new int[width][height];
         g = new int[width][height];
         b = new int[width][height];
 
-        for (int x = 0; x < width; x++){
-            for (int y = 0; y < height; y++){
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
                 int rgb = image.getRGB(x, y);
                 r[x][y] = (rgb >> 16) & 0xFF;
                 g[x][y] = (rgb >> 8) & 0xFF;
@@ -25,4 +27,50 @@ public class Image {
             }
         }
     }
+
+    public Image(int r[][], int g[][], int b[][]) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.width = r.length;
+        this.height = r[0].length;
+    }
+
+    public static BufferedImage toBufferedImage(Image i) {
+        BufferedImage image = new BufferedImage(i.width, i.height, BufferedImage.TYPE_INT_RGB);
+        for (int x = 0; x < i.width; x++) {
+            for (int y = 0; y < i.height; y++) {
+                int rgb = i.r[x][y];
+                image.setRGB(x, y, rgb);
+            }
+        }
+        return image;
+    }
+    
+
+    public static Image displayFourier(Complex[][] freqData) {
+        int rows = freqData.length;
+        int cols = freqData[0].length;
+        int[][] grayscale = new int[rows][cols];
+        double maxMag = 0.0;
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                double mag = freqData[r][c].magnitude();
+                if (mag > maxMag) {
+                    maxMag = mag;
+                }
+            }
+        }
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                double mag = freqData[r][c].magnitude();
+                double val = Math.log1p(mag) / Math.log1p(maxMag);
+                int intensity = (int) Math.round(val * 255);
+                int color = (intensity << 16) | (intensity << 8) | intensity;
+                grayscale[r][c] = color;
+            }
+        }
+        return new Image(grayscale, grayscale, grayscale);
+    }
+
 }
--- src/processors/FourierProcessor.java
+++ src/processors/FourierProcessor.java
@@ -10,4 +10,68 @@ public class FourierProcessor {
     public static Image decode(Image decode) {
         return decode;
     }
+
+    public static Complex[] oneDimensionalDFT(Complex[] source) {
+        int N = source.length;
+        Complex[] output = new Complex[N];
+
+        // Initialize output array
+        for (int k = 0; k < N; k++) {
+            output[k] = new Complex(0, 0);
+        }
+
+        for (int k = 0; k < N; k++) {
+            for (int n = 0; n < N; n++) {
+                double angle = -2 * Math.PI * k * n / N;
+                double cosA = Math.cos(angle);
+                double sinA = Math.sin(angle);
+                double realPart = source[n].real * cosA - source[n].imag * sinA;
+                double imagPart = source[n].real * sinA + source[n].imag * cosA;
+
+                // Accumulate in the output
+                output[k].real += realPart;
+                output[k].imag += imagPart;
+            }
+        }
+
+        return output;
+    }
+
+    public static Complex[][] twoDimensionalDFT(double[][] image) {
+        int rows = image.length;
+        int cols = image[0].length;
+
+        Complex[][] complexImage = new Complex[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                complexImage[i][j] = new Complex(image[i][j], 0);
+            }
+        }
+
+        // compute 1d fft on each row
+        Complex[][] rowTransformed = new Complex[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            rowTransformed[i] = oneDimensionalDFT(complexImage[i]);
+        }
+
+        // compute on each col
+        Complex[][] finalTransform = new Complex[rows][cols];
+
+        for (int j = 0; j < cols; j++) {
+
+            Complex[] column = new Complex[rows];
+            for (int i = 0; i < rows; i++) {
+                column[i] = rowTransformed[i][j];
+            }
+
+            Complex[] columnDFT = oneDimensionalDFT(column);
+
+            // Store results in the final transformation matrix
+            for (int i = 0; i < rows; i++) {
+                finalTransform[i][j] = columnDFT[i];
+            }
+        }
+
+        return finalTransform;
+    }
 }
