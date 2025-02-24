8d94d50fd277755e1531de58fe5a8205fe0a0d95
jdugan0 <justin.d.dugan@gmail.com>
2025-02-24 15:08:23 -0500

    added jtransform

--- src/processors/FourierProcessor.java
+++ src/processors/FourierProcessor.java
@@ -10,22 +10,4 @@ public class FourierProcessor {
     public static Image decode(Image decode) {
         return decode;
     }
-
-    public static double[][] applyFFT(int[][] channel) {
-        int width = channel.length;
-        int height = channel[0].length;
-        Complex[][] fftData = new Complex[width][height];
-        // Convert integer data to double for FFT
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
-                
-            }
-        }
-
-        // Apply 2D FFT
-        DoubleFFT_2D fft = new DoubleFFT_2D(width, height);
-        fft.complexForward(fftData);
-
-        return fftData;
-    }
 }
