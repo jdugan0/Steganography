b7f98b98906a7e62d2b642e5a04c81b4378c9f7d
jdugan0 <justin.d.dugan@gmail.com>
2025-03-04 15:08:49 -0500

    worky worm

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,17 @@
 import filereader.FileReader;
 import filereader.FileReader.ImageType;
 import filereader.Image;
-import processors.FourierProcessor;
+import processors.FourierSteganographyColor;
 
 public class Main {
     public static void main(String[] args) {
-        Image encoded = FourierProcessor.encode(FileReader.readImage(ImageType.Store, "store.png"),
-                FileReader.readImage(ImageType.Encode, "encode.png"), 0.05);
+        Image encoded = FourierSteganographyColor.encode(FileReader.readImage(ImageType.Store, "store.png"),
+                FileReader.readImage(ImageType.Encode, "encode.png"), 0.2);
 
         FileReader.writeImage(Image.toBufferedImage(encoded), ImageType.Output, "encoded.png");
 
-        Image decoded = FourierProcessor.decode(encoded, 0.05);
+        Image decoded = FourierSteganographyColor.decode(encoded, (int) (encoded.width * 0.2),
+                (int) (encoded.height * 0.2));
 
         FileReader.writeImage(Image.toBufferedImage(decoded), ImageType.Output, "decoded.png");
     }
