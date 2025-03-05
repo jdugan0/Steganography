2262e8c543ae50de0624a245f084a77a48e3e5ea
jdugan0 <justin.d.dugan@gmail.com>
2025-03-05 11:26:12 -0500

    blind and nonblind

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 import filereader.FileReader;
 import filereader.FileReader.ImageType;
 import filereader.Image;
-import processors.FourierSteganographyColor;
+import processors.FourierBlind;
 
 public class Main {
     public static void main(String[] args) {
-        Image encoded = FourierSteganographyColor.encode(FileReader.readImage(ImageType.Store, "store.png"),
-                FileReader.readImage(ImageType.Encode, "encode.png"), 0.2);
+        Image encoded = FourierBlind.encode(FileReader.readImage(ImageType.Store, "store.png"),
+                FileReader.readImage(ImageType.Encode, "encode.png"), 0.25);
 
         FileReader.writeImage(Image.toBufferedImage(encoded), ImageType.Output, "encoded.png");
 
-        Image decoded = FourierSteganographyColor.decode(encoded, (int) (encoded.width * 0.2),
-                (int) (encoded.height * 0.2));
+        Image decoded = FourierBlind.decode(encoded, (int) (encoded.width * 0.25),
+                (int) (encoded.height * 0.25));
 
         FileReader.writeImage(Image.toBufferedImage(decoded), ImageType.Output, "decoded.png");
     }
